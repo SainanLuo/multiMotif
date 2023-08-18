5cd0ecd9f38b9adab03171f785563cb3814665f9
Luo-Sainan <93503397+Luo-Sainan@users.noreply.github.com>
2023-08-18 12:33:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='variamotif',
-    version='0.1',
+    version='0.2.1',
     packages=find_packages(),
     install_requires=[
         'biopython>=1.78',
