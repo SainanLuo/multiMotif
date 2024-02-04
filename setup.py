3210e5bcafc82732bde4628e5b00e1130dd24548
Sienna <93503397+Sienna-L@users.noreply.github.com>
2024-02-04 16:42:11 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='multiMotif',
-    version='1.0.5',
+    version='1.0.8',
     packages=find_packages(),
     install_requires=[
         'biopython>=1.78',
