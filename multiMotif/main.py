02cc0d9d0f0043d4e4fbd8d30c24089b8de6f918
Sienna <93503397+Sienna-L@users.noreply.github.com>
2024-07-03 23:32:20 +0800

    update

--- multiMotif/main.py
+++ multiMotif/main.py
@@ -174,7 +174,6 @@ def singleMotif_function(args):
                 result['motif_type'] = "singleMotif"
                 filtered_results.append(result)
 
-
     if args.protein:
         logging.info("Starting single motif scanning on protein records.")
         motif_results = pool.starmap(search_motif_protein, [(record, motif, max_mismatches, len(motif)) for record in records])
@@ -187,7 +186,7 @@ def singleMotif_function(args):
         logging.info("Single motif scanning found no results.")
     else:
         with open(output_file_path1,'w') as output_file:
-            output_file.write("Sequence_ID\tSequence_Length\tmotif\tmotif_type\tstart\tend\tstrand\tmismatches\tmatched_sequence\n")
+            output_file.write("sequence_id\tsequence_length\tmotif\tmotif_type\tstart\tend\tstrand\tmismatches\tmatched_sequence\n")
             for result in filtered_results:
                 result['motif']=f"single_{result['motif']}"
                 output_file.write(f"{result['sequence_id']}\t{result['sequence_length']}\t{result['motif']}\t{result['motif_type']}\t{result['start']}\t{result['end']}\t{result['strand']}\t{result['mismatches']}\t{result['fragment']}\n")
@@ -249,7 +248,7 @@ def singleMotif_function(args):
                 fimo_results_filter.append(result)
 
         with open(output_file_path2,'w') as output_file:
-            output_file.write("Sequence_ID\tSequence_Length\tmotif\tmotif_type\tstart\tend\tstrand\tp-value\tq-value\tmatched_sequence\n")
+            output_file.write("sequence_id\tsequence_length\tmotif\tmotif_type\tstart\tend\tstrand\tp-value\tq-value\tmatched_sequence\n")
             for result in fimo_results_filter:
                 output_file.write(f"{result['sequence_id']}\t{result['sequence_length']}\t{result['motif']}\t{result['motif_type']}\t{result['start']}\t{result['end']}\t{result['strand']}\t{result['p-value']}\t{result['q-value']}\t{result['fragment']}\n")
         logging.info("Starting to visualize FIMO results.")
@@ -277,7 +276,7 @@ def singleMotif_function(args):
 
 
         with open(output_file_path3,'w') as output_file:
-            output_file.write("Sequence_ID\tSequence_Length\tmotif\tmotif_type\tstart\tend\tstrand\n")
+            output_file.write("sequence_id\tsequence_length\tmotif\tmotif_type\tstart\tend\tstrand\n")
             for result in merged_results:
                 output_file.write(f"{result['sequence_id']}\t{result['sequence_length']}\t{result['motif']}\t{result['motif_type']}\t{result['start']}\t{result['end']}\t{result['strand']}\n")
         logging.info("Starting to visualize the comparison results between singleMotif and FIMO.")
@@ -354,7 +353,7 @@ def multiMotif_function(args):
             logging.info("Multiple motifs scanning found no results.")
         else:
             with open(output_file_path1,'w') as output_file:
-                output_file.write("Sequence_ID\tSequence_Length\tmotif\tmotif_type\tstart\tend\tstrand\tmismatches\tmatched_sequence\n")
+                output_file.write("sequence_id\tsequence_length\tmotif\tmotif_type\tstart\tend\tstrand\tmismatches\tmatched_sequence\n")
                 for result in all_results:
                     output_file.write(f"{result['sequence_id']}\t{result['sequence_length']}\t{result['motif']}\t{result['motif_type']}\t{result['start']}\t{result['end']}\t{result['strand']}\t{result['mismatches']}\t{result['fragment']}\n")
             logging.info("Multiple motifs scanning is completed.")
@@ -380,7 +379,15 @@ def multiMotif_function(args):
         filtered_results_sorted = sorted(filtered_results, key=lambda x: (x['sequence_id'],x['start']))
         grouped_results = groupby(filtered_results_sorted, key=lambda x: x['sequence_id'])
         grouped_results_list = [(key,list(group)) for key, group in grouped_results]
-        manyMotifs_results_pre1 = pool.starmap(filter_rows, [(group, motif_nums,line_count,args.necessary) for key, group in grouped_results_list])
+
+        if args.necessary:
+            with open(args.necessary, 'r') as file:
+                motif_must = set(int(item) for item in file.readline().strip().split(','))
+        else:
+            motif_must=set()
+
+        manyMotifs_results_pre1 = pool.starmap(filter_rows, [(group, motif_nums,line_count,motif_must) for key, group in grouped_results_list])
+
         manyMotifs_results_pre2 = [item for sublist in manyMotifs_results_pre1 if sublist is not None for item in sublist]    
         manyMotifs_results.extend(manyMotifs_results_pre2)
         manyMotifs_results= [item for sublist in manyMotifs_results if sublist is not None for item in sublist]
@@ -390,7 +397,7 @@ def multiMotif_function(args):
             logging.info("After the positional order check, no results met the criteria.")
         else:
             with open(output_file_path2,'w') as output_file:
-                output_file.write("Sequence_ID\tSequence_Length\tmotif\tmotif_type\tstart\tend\tstrand\tmismatches\tmatched_sequence\n")
+                output_file.write("sequence_id\tsequence_length\tmotif\tmotif_type\tstart\tend\tstrand\tmismatches\tmatched_sequence\n")
                 for result in manyMotifs_results:
                     output_file.write(f"{result['sequence_id']}\t{result['sequence_length']}\t{result['motif']}\t{result['motif_type']}\t{result['start']}\t{result['end']}\t{result['strand']}\t{result['mismatches']}\t{result['fragment']}\n")
             logging.info("Positional order check is completed.")
@@ -480,7 +487,7 @@ def multiMotif_function(args):
             logging.info("Variable gap motifs scannin found no results.")
         else:
             with open(output_file_path, 'w') as output_file:
-                output_file.write("Sequence_ID\tSequence_Length\tMotif\tmotif_type\tstart\tend\tstrand\tmotif1_mismatch\tfragment1\tmotif2_mismatch\tfragment2\tmismatches\tgap_length\tmatched_sequence\n")
+                output_file.write("sequence_id\tsequence_length\tMotif\tmotif_type\tstart\tend\tstrand\tmotif1_mismatch\tfragment1\tmotif2_mismatch\tfragment2\tmismatches\tgap_length\tmatched_sequence\n")
                 for result in all_results:
                     output_file.write(f"{result['sequence_id']}\t{result['sequence_length']}\t{result['motif']}\t{result['motif_type']}\t{result['start']}\t{result['end']}\t{result['strand']}\t{result['motif1_mismatch']}\t{result['fragment1']}\t{result['motif2_mismatch']}\t{result['fragment2']}\t{result['mismatch']}\t{result['gap_length']}\t{result['sequence']}\n")
         if args.image:
@@ -551,6 +558,7 @@ def extract_sequences_function(args):
                         continue
                     seq_record = SeqRecord(seq_fragment, id=f"{seq_name}|{cds_id}|{parent}|{gene}|{seq_start}|{seq_end}|{strand}|{product}",description="")
                     SeqIO.write(seq_record, output_handle, "fasta")
+
     output_handle.close()
     logging.info("Done")
 
@@ -911,6 +919,11 @@ def check_sequential_numbers(nums,sorted_nums, m):
         if nums[i] in processed_nums:
             i += 1
             continue
+
+
+        if count >= len(sorted_nums):
+            break
+
         if nums[i] == sorted_nums[count]:
             ordered_nums.append((nums[i], i))
             processed_nums.add(nums[i])
@@ -937,20 +950,21 @@ def check_sequential_numbers(nums,sorted_nums, m):
     else:
         return None
 
-def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
+def filter_rows(result_list, motif_nums, line_count,motif_must=set()):
+
     if len(set(row['motif_type'] for row in result_list)) < motif_nums:
         return None
 
     temp = []
     lists = []
     i = 0
-
     filtered_rows = []
-
     num_list = []
+    num_list_index=[]
     num_list_add=[]
     nums=[]
     sorted_nums =[]
+
 
     count_positive_strand = 0
     count_negative_strand = 0
@@ -959,10 +973,12 @@ def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
             count_positive_strand += 1  
         else:
             count_negative_strand += 1
+
     if count_positive_strand > count_negative_strand:
+
         while i < len(result_list) -1:
             temp.append((result_list[i], i))
-            if result_list[i]['motif_type'] >= result_list[i + 1]['motif_type'] or result_list[i+1]['start'] - result_list[i]['end'] > (result_list[i + 1]['motif_type'] - result_list[i]['motif_type'])*1000:  #一旦motif类型不是增长，就结束这个小列表
+            if result_list[i]['motif_type'] >= result_list[i + 1]['motif_type'] or result_list[i+1]['start'] - result_list[i]['end'] > (result_list[i + 1]['motif_type'] - result_list[i]['motif_type'])*1000:
                 lists.append(temp)
                 temp = []
             i += 1
@@ -973,14 +989,18 @@ def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
 
         for sublist in sorted(lists, key=len, reverse=True):
             filtered_temp=[]
-            if any(all(item in sublist for item in sublist_item) for sublist_item in num_list_add):
+
+            if any(all(item in sublist_item for item in sublist) for sublist_item in num_list_add):
                 continue
 
+
             if sublist[0][0]['motif_type'] > 1:
+
                 up = ((sublist[0][0]['motif_type'] - 1) // 3 + 1) * 1000
                 up_value = sublist[0][0]['start'] - up
+
                 if up_value < result_list[0]['start']:
-                    f_start=1
+                    f_start=0
                 else:
                     for sub_list in lists:
                         if sub_list[0][0]["start"] <= up_value <= sub_list[-1][0]['end']:
@@ -991,6 +1011,7 @@ def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
                             break
             else:
                 f_start = sublist[0][1]
+
             if sublist[-1][0]['motif_type'] < line_count:
                 down = ((int(line_count - sublist[-1][0]['motif_type'])) // 3 + 1) * 1000
                 down_value = sublist[-1][0]['end'] + down
@@ -1011,10 +1032,13 @@ def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
                 break
 
             num_list = result_list[f_start:g_end + 1]
-            num_list_add.append(num_list)
+            num_list_index = [(result_list[i], i) for i in range(f_start, g_end + 1)]
+            num_list_add.append(num_list_index)
             nums = [row['motif_type'] for row in num_list]
             sorted_nums = sorted(set(nums))
+
             result_index = check_sequential_numbers(nums, sorted_nums, motif_nums)
+
             if result_index is None:
                 if filtered_rows:
                     return filtered_rows
@@ -1025,20 +1049,17 @@ def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
                 for i in result_index:
                     filtered_temp.append(num_list[i])
 
-                if necessary_file:
-                    with open('file1.txt', 'r') as file:
-                        motif_must = file.readline().strip().split(',')
-                    data_list = [row['motif_type'] for row in filtered_temp]
-                    all_in = all(item in data_list for item in motif_must)
-
-                    if all_in:
+                if motif_must:
+                    data_list = set([row['motif_type'] for row in filtered_temp])
+                    if motif_must.issubset(data_list):
                         filtered_rows.append(filtered_temp)
                     else:
-                        return filtered_rows
-
+                        if filtered_rows:
+                            return filtered_rows
+                        else:
+                            return None
                 else:
                     filtered_rows.append(filtered_temp)
-
 
     else:
         while i < len(result_list) -1:
@@ -1054,14 +1075,17 @@ def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
 
         for sublist in sorted(lists, key=len, reverse=True):
             filtered_temp=[]
-            if all(item in num_list for item in sublist):
+
+            if any(all(item in sublist_item for item in sublist) for sublist_item in num_list_add):
                 continue
+
 
             if sublist[0][0]['motif_type'] < line_count:
                 up = ((line_count-sublist[0][0]['motif_type'] ) // 3 + 1) * 1000
                 up_value = sublist[0][0]['start'] - up
                 if up_value < result_list[0]['start']:
-                    f_start=1
+                    f_start=0
+
                 else:
                     for sub_list in lists:
                         if sub_list[0][0]["start"] <= up_value <= sub_list[-1][0]['end']:
@@ -1093,10 +1117,13 @@ def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
                 break
 
             num_list = result_list[f_start:g_end + 1]
+            num_list_index = [(result_list[i], i) for i in range(f_start, g_end + 1)]
+            num_list_add.append(num_list_index)
             nums = [row['motif_type'] for row in num_list]
             sorted_nums = sorted(set(nums),reverse=True)
 
             result_index = check_sequential_numbers(nums, sorted_nums, motif_nums)
+
             if result_index is None:
                 if filtered_rows:
                     return filtered_rows
@@ -1107,15 +1134,15 @@ def filter_rows(result_list, motif_nums, line_count,necessary_file=None):
                 for i in result_index:
                     filtered_temp.append(num_list[i])
 
-                if necessary_file:
-                    with open('file1.txt', 'r') as file:
-                        motif_must = file.readline().strip().split(',')
-                    data_list = [row['motif_type'] for row in filtered_temp]
-                    all_in = all(item in data_list for item in motif_must)
-                    if all_in:
+                if motif_must:
+                    data_list = set([row['motif_type'] for row in filtered_temp])
+                    if motif_must.issubset(data_list):
                         filtered_rows.append(filtered_temp)
                     else:
-                        return filtered_rows
+                        if filtered_rows:
+                            return filtered_rows
+                        else:
+                            return None
                 else:
                     filtered_rows.append(filtered_temp)
     return filtered_rows
@@ -1212,18 +1239,21 @@ def collate_bed(correction_gff, seq_id, min_length,option=None, start=None,end=N
 def plot_motifs_to_single_chart(file_path, output_file, display_both_directions=False, gff_file=None, motif_up=2000, motif_down=2000):
     df = pd.read_csv(file_path, sep='\t', header=None, dtype={0: str, 1: int, 2: str, 3: str, 4: int, 5: int, 6: str}, usecols=[0, 1, 2, 3, 4, 5, 6], skiprows=1, names=["Sequence ID", "Length", "Motif", "motif_type", "Start", "End", "Strand"])
     grouped_df = df.groupby("Sequence ID")
+
     if df["Length"].min()>50000:
         min_length = min(50000,df["Length"].min())
     else:
         min_length = 50000
 
     correction_gff=[]
+
     if gff_file:
         correction_gff = collate_gff(gff_file)
 
     gene_lists=[]
 
     for idx, (seq_id, seq_group) in enumerate(grouped_df):
+
         min_start_end = pd.concat([seq_group["Start"], seq_group["End"]]).min()
         max_start_end = pd.concat([seq_group["Start"], seq_group["End"]]).max()
         if (max_start_end - min_start_end > min_length):
@@ -1232,6 +1262,7 @@ def plot_motifs_to_single_chart(file_path, output_file, display_both_directions=
                 gene_lists.extend(gene_list)
             for index, row in seq_group.iterrows():
                 df.loc[index, "Sequence ID"] = row["Sequence ID"] + '|' + str(math.floor(row["End"]/min_length)*min_length)
+                
                 df.loc[index, "Length"] = min_length
                 if row['Strand'] == "+":
                     df.loc[index, "Start"] = row["Start"] % min_length
@@ -1257,6 +1288,7 @@ def plot_motifs_to_single_chart(file_path, output_file, display_both_directions=
             for index, row in seq_group.iterrows():
                 df.loc[index, "Length"] = end_to - start_from
                 df.loc[index, "Start"] = row["Start"] - start_from
+
                 df.loc[index, "End"] = row["End"] - start_from
                 df.loc[index, "start_from"] = start_from
                 df.loc[index, "end_to"] = end_to
@@ -1270,32 +1302,36 @@ def plot_motifs_to_single_chart(file_path, output_file, display_both_directions=
     grouped_again_df = df.groupby("Sequence ID", sort=False)
     num_sequences = len(grouped_again_df)
     height_interval = 0.7
+
     max_length = df["Length"].max()
     min_height = 300
     if num_sequences <= 2:
         height = min_height
     else:
         height = 100 * num_sequences
+
     screen_width = 1920
     max_length_proportion = 0.7
     line_length = screen_width * max_length_proportion
     multiplier = line_length / max_length
+
     color_list = ['#008000','#FF0000', '#0000FF', '#FF00FF', '#00FFFF', '#FFFF00', '#800000', '#00FF00', '#000080', '#808000', '#800080', '#008080', '#808080', '#C0C0C0', '#FFA500', '#E64B35','#4DBBD5','#3C5488','#F39B7F','#91D1C2','#925E9F','#84BD00','#8C564B','#5C88DA','#D6616B','#6B58EE','#FDD0A2','#E7BA52','#3F4041','#9632B8']
     color_map = {}
 
     if gff_file:
-
         fig = make_subplots(rows=num_sequences, cols=1, shared_xaxes=True)
-
         for idx, (seq_id, seq_group) in enumerate(grouped_again_df):
             y_position = idx * 2
             seq_length = seq_group.iloc[0]["Length"]
             line = seq_length * multiplier
+
+
             fig.add_trace(go.Scatter(x=[0, line], y=[y_position, y_position], mode='lines', line=dict(color='black'), showlegend=False),row=idx+1,col=1)
             fig.add_annotation(x=-0.5, y=y_position, text=seq_id, showarrow=False, font=dict(size=15), xanchor='right', yanchor='middle',row=idx+1,col=1)
             tick_positions = np.linspace(0, line, num=11)
             if (seq_group.iloc[0]["type"]=="A"):   
                 tick_labels = np.linspace(seq_group.iloc[0]["n"], seq_group.iloc[0]["n"]+min_length, num=11, dtype=int)
+ 
             elif(seq_group.iloc[0]["type"]=="B"):
                 tick_labels = np.linspace(seq_group.iloc[0]["start_from"],seq_group.iloc[0]["end_to"], num=11, dtype=int)                       
             else:
